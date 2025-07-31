674e7c15d47ae928dd1f4845cfca957099954949
6enard <109796647+6enard@users.noreply.github.com>
2025-07-31 19:13:39 +0300

    Add missing closing nav tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -132,6 +132,7 @@ const Header = () => {
             </button>
           </div>
         </div>
+      </nav>
 
         {/* Mobile Nav Menu - moved outside of nav container */}
         {isMenuOpen && (
